7353ea2f3119c3e0a2e66a74b1725965b916aa15
onura2 <50609671+onura2@users.noreply.github.com>
2019-05-14 08:17:14 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('message', msg => {
+  if (msg.content === 'hawk') {
+    msg.reply('efendim  ');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'sa') {
+    msg.reply('as');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'nasılsın') {
+    msg.reply('iyi sen ');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'bu barın sahibi kim ') {
+    msg.reply('meliodas bende yardımcısı');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'menu') {
+    msg.reply('biri menu mu dedi hhhh ');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'keserim seni') {
+    msg.reply('ozaman kacar ben ::F   ');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'garson') {
+    msg.reply('yemek geldi geldi konusan domuz gordune sasırma:D');
+  }
+});
+client.login('NTc3MDMzMTE3Nzg1Nzg0MzMw.XNf6gg.KkiIQcZzyVWrsNZXFTPp__BGJVE');
